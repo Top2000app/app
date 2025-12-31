90b78ae00b2d3d07d372235c1f6301bbea8fbaa6
Rick Neeft <rick.neeft@outlook.com>
2025-12-31 23:31:44 +0100

    Blazor and other things

--- src/apps/Top2000.Apps.BlazorApp2/Program.cs
+++ src/apps/Top2000.Apps.BlazorApp2/Program.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Top2000.BlazorApp;
 using MudBlazor.Services;
-using Top2000.Features;
+using Top2000.Apps.BlazorApp2;
+using Top2000.Apps.MudBlazorApp;
 using Top2000.Features.Json;
+using Top2000.Features;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,20 +12,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services
     .AddMudServices()
-    .AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
     .AddTop2000Features<JsonFeatureAdapter>(configure =>
     {
         configure.ConfigureDataLoader(services =>
         {
             services.AddTransient<IDataLoader, Top2000DataLoader>();
         });
-    })
-    ;
+    });
 
-var app = builder.Build();
-
-// Initialize TrackService before running the app
-var trackService = app.Services.GetRequiredService<Top2000Services>();
-await trackService.InitialiseDataAsync();
-
-await app.RunAsync();
+await builder.Build().RunAsync();
---
+++ src/features/Top2000.Features.Json.Tests/Test1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Testing.Platform.Extensions.Messages;
+
+namespace Top2000.Features.Json.Tests;
+
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public async Task TestMethod1()
+    {
+        var top2000 = new ServiceCollection()
+            .AddTop2000Features<JsonFeatureAdapter>(configure =>
+            {
+                configure.ConfigureDataLoader(services =>
+                {
+                    services.AddSingleton<IDataLoader, DataLoader>();
+                });
+            })
+            .BuildServiceProvider()
+            .GetRequiredService<Top2000Services>();
+
+        await top2000.InitialiseDataAsync();
+        var editions = await top2000.AllListingsOfEditionAsync(2025);
+        
+        Assert.IsNotNull(editions);
+        Assert.IsTrue(editions.Any());
+    }
+}
+
+public class DataLoader : IDataLoader
+{
+    public Task<Stream> LoadDataAsync(CancellationToken cancellationToken = default)
+    {
+        var stream = File.OpenText("top2000.json");
+        return Task.FromResult(stream.BaseStream);
+    }
+}

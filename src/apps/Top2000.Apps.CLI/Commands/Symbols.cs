39cf51e49b66934825daffda93b3957cc2a03ec3
Rick Neeft <rick.neeft@outlook.com>
2026-01-16 21:39:52 +0100

    Windows uni code

--- src/apps/Top2000.Apps.CLI/Commands/Symbols.cs
+++ src/apps/Top2000.Apps.CLI/Commands/Symbols.cs
@@ -2,13 +2,14 @@ namespace Top2000.Apps.CLI.Commands;
 
 public static class UnicodeSymbols
 {
-    public static string Check => OperatingSystem.IsWindows() ? "[Y]" : "✔";
-    public static string Wrong => OperatingSystem.IsWindows() ? "[N]" : "✗";
+    public static string Check => OperatingSystem.IsWindows() ? "√" : "✔";
+    public static string Wrong => OperatingSystem.IsWindows() ? "x" : "✗";
     public static string Equal => "=";
-    public static string Up => OperatingSystem.IsWindows() ? "^" : "↑";
-    public static string Down => OperatingSystem.IsWindows() ? "v" : "↓";
-    public static string New => OperatingSystem.IsWindows() ? "* " : "⚑";
-    public static string Recurring => OperatingSystem.IsWindows() ? "~" : "↻";
+    public static string Up => OperatingSystem.IsWindows() ? "↑" : "↑";
+    public static string Down => OperatingSystem.IsWindows() ? "↓" : "↓";
+    public static string New => OperatingSystem.IsWindows() ? "♫ " : "⚑";
+    public static string Recurring => OperatingSystem.IsWindows() ? "↔" : "↻";
     public static string Dash => OperatingSystem.IsWindows() ? "-" : "–";
-    public static string Delta => OperatingSystem.IsWindows() ? "D" : "Δ";
+    public static string Delta => OperatingSystem.IsWindows() ? "∆" : "Δ";
 }
+
